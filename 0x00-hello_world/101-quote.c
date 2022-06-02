b4a33be652c58e6ee1231826868d77af9b178d0e
angeliah <angeliaowango@gmail.com>
2022-06-02 14:01:08 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-  * main - entry point
+  * main - Prints and that piece of art is useful - Dora Korpar, 2015-10-19,
+  *   folowed by a new line,to the standard error.
   *
   * Return Always 1 (Success)
   */
 int main(void)
 {
-        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		  59);
 	return (1);
 }
