014a70adebc2af95c250d11b5b8ba280bd5ed89c
angeliah <angeliaowango@gmail.com>
2022-06-03 07:19:06 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 
-/**main - prints the alphabet in lowercase
+/**
+  *maiN- entry point
+  *
+  *Description: prints the alphabet in lowercase
   *
   * Return : 0 on success
   */
@@ -8,7 +11,7 @@ int main(void)
 {
 	char c = 'a';
 
-	while( c <= 'z')
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
