03be7d3cbee611c724d4e8715689af8b5bc29f9a
angeliah <angeliaowango@gmail.com>
2022-06-08 16:43:50 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,6 +20,7 @@ void print_to_98(int n)
 			}
 			n++;
 		}
+	}
 		else if (n > 98)
 		{
 			while (n >= 98)
@@ -37,5 +38,4 @@ void print_to_98(int n)
 			printf("98");
 		}
 		printf("\n");
-	}
 }
