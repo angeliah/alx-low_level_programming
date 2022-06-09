20c477ce19ae8ee121a14b7ab245513d8bc47ed4
angeliah <angeliaowango@gmail.com>
2022-06-09 10:12:50 -0700

    removing white space

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-  *main - tests that the function positive_or_negative() 
-  *gives the correct output when given a case of 0.
+  *main - test that the function positive_or_negative()i
+  * gives the correct output when given a case of 0.
   *
   *Return: 0(success)
   */
