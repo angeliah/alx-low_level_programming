de82f9ff627ccff75352c33164b3e14d64f9ddd6
angeliah <angeliaowango@gmail.com>
2022-06-09 10:04:47 -0700

    correcting betty doc

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-  *main - tests function that prints if integer is positive or negative
+  *main - tests that the function positive_or_negative() 
+  *gives the correct output when given a case of 0.
   *
   *Return: 0(success)
   */
