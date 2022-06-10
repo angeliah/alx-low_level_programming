58a8658270858f134b04adffcfe577b6e75f9f10
angeliah <angeliaowango@gmail.com>
2022-06-10 16:08:00 -0700

    updatng 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: always o
+ */
+int main(void)
+{
+	 print_triangle(2);
+	 print_triangle(10);
+	 print_triangle(1);
+	 return (0);
+}
+
