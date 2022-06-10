0953bc1be23ca5c91670c8e8a0cf52a28b93396a
angeliah <angeliaowango@gmail.com>
2022-06-10 06:31:20 -0700

    correction of 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *-mul - function that multiplies two integers
+  *@a: first input value
+  *@b: second input value
+  *
+  *Return: result of mul
+  */
+int mul(int a, int b)
+{
+	return (a*b);
+}
