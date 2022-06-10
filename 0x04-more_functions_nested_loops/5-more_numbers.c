80f60974fa4ec3c18d280c87d89ed6eefcf496e4
angeliah <angeliaowango@gmail.com>
2022-06-10 14:21:05 -0700

    correction of 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,12 @@
 void more_numbers(void)
 {
 	int i;
-	int n;
+	int j;
 
-	i = 0;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-	       for (i = 0; i <= 14; i++)
+	       for (i= 0; i <= 14; i++)
 	       {
 		       if (i >= 10)
 		       {
