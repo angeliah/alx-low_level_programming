e5699c3422002fa28dca506eadd9c5c79044b51b
angeliah <angeliaowango@gmail.com>
2022-06-13 14:48:40 -0700

    updating 4-print-rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
 
-	while (s[index++])
-		len++;
-	for (index = len - 1; index >= 0; index--)
+	while (s[index] != '\0')
+		index++;
+	for (index -= 1; index >= 0; index--)
 		_putchar(s[index]);
+
 	_putchar('\n');
 }
