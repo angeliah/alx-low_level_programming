b99e1b4ef7d9a57c1539f401541ce178482660de
angeliah <angeliaowango@gmail.com>
2022-06-13 13:17:38 -0700

    function that rints the lenght of a string;

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string whose lngth is to be returned
+ *
+ * Return: always 0 (success)
+ */
+int _strlen(char *s)
+{
+	_putchar (sizeof(s));
+	return (0);
+}
