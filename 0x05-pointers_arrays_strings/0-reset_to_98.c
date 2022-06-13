36146e48bd1b925025c4155a215aa14507e47399
angeliah <angeliaowango@gmail.com>
2022-06-13 09:04:41 -0700

    updatin betty doc style for 0-98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and 
+ *reset_to_98 - function that takes a pointer to an int as parametr and
  * update the value to 98.
  * @n:
  * Value to be replaced
