cf80c7296c3820e63dac190fcb4a6d59b94250aa
angeliah <angeliaowango@gmail.com>
2022-06-13 15:12:56 -0700

    fixin 5-rev-string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int len = 0; index;
+	int len = 0; index = 0; 
+	char tmp;
 
 	while (s[index] != '\0')
 		len++;
 
-	for (len = index - 1; index >= 0 ; index--)
-		_putchar (s[index]);
+	for (len = index - 1; index >= len / 2 ; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	
 	_putchar('\n');
 }
