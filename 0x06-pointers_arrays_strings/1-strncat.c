0e2f965432b070e653e5c222dc20c3d1d5de35d1
angeliah <angeliaowango@gmail.com>
2022-06-15 11:49:40 -0700

    updating 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,9 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while ((*dest + i) != '\0')
+	while (*(dest + i) != '\0')
 		i++;
-	while ((*src + j) != '\0')
+	while (*(src + j) != '\0')
 	{
 		if (j < n)
 		{
