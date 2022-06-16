5df610cc2b11c8fd3a76ce18a24967d43ce77331
angeliah <angeliaowango@gmail.com>
2022-06-16 07:21:56 -0700

    fix 2 of rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,16 +2,16 @@
 
 /**
  * rot13 - function that encodes a string using rot13.
- * @c: pointer to char
+ * @s: pointer to char
  * Return: char
  */
 char *rot13(char *s)
 {
-	int i = 0;
+int i = 0;
 	int j = 0;
 	char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 	char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-	
+
 	while (*(s + i) != '\0')
 	{
 		while (*(l + j) != '\0')
