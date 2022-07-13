2998ca8805eb87a18674360e8971f1fcb6a447e5
Angelia owango <44548054+angeliah@users.noreply.github.com>
2022-07-13 16:40:11 +0300

    Fix 3 of print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,20 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	{
 	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[j][i]);
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
 		}
-		_putchar('\n');
-
 	}
 }
