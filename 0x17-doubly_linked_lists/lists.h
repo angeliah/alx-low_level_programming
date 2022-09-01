cc97a026bcb0a3b2f4d512d7e953e17d9d0d1340
angeliah <angeliaowango@gmail.com>
2022-09-01 11:42:45 -0700

    updating header file lists.h by adding structure to be used for the project

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,31 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*included libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Definitions */
+
+/**struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for ALX project
+ *
+ */
+
+ typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+}dlistint_t;
+
+/*Function prototypes */
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
