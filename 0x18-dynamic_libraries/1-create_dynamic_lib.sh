a819584f75928802aa1c4830450dbe619491a5b1
angeliah <angeliaowango@gmail.com>
2022-09-05 13:41:04 -0700

    Another fix to file 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,1 @@
-gcc *.c -c -fpic
-gcc *.o -shared -o liball.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+gcc *.c -c -fpic|gcc *.o -shared -o liball.so|export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
