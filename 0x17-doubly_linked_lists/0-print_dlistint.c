e8f9a7497f1444556a8a7d7439b150e2270716b5
angeliah <angeliaowango@gmail.com>
2022-09-09 09:05:48 -0700

    Correcting betty for file 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	 while (h != NULL)
-	 {
-		 printf("%d\n", h->n);
-		 h = h->next;
-		 i++;
-	 }
-	 return (i);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
