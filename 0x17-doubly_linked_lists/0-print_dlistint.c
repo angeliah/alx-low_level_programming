7fc9da9577b6907a464dd04c1855fd8d11e3d286
angeliah <angeliaowango@gmail.com>
2022-09-09 08:56:04 -0700

    Correcting file 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,14 @@
  *
  */
 size_t print_dlistint(const dlistint_t *h)
-{	
-	size_t n;
-	 while (n != NULL)
-	 {
-		 printf("%d\n", h->head)
-			 new = h;
-		 	 h = h ->next;
-	 }
-}
+{
+	size_t i = 0;
 
+	 while (h != NULL)
+	 {
+		 printf("%d\n", h->n);
+		 h = h->next;
+		 i++;
+	 }
+	 return (i);
+}
