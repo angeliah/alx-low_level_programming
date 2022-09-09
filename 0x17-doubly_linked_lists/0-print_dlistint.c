da8b7e920d2743c1e6471052e253bac557d9ab57
angeliah <angeliaowango@gmail.com>
2022-09-09 08:12:24 -0700

    Update of file 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,13 @@
  *
  */
 size_t print_dlistint(const dlistint_t *h)
-
-{
-	size_t count = 0;
-	dlitint_t *temp = (dlistint_t *)h;
-
-	if (h == NULL)
-		return  (0);
-	while (temp != NULL)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-		count++;
-	}
-	return (count);
+{	
+	size_t n;
+	 while (n != NULL)
+	 {
+		 printf("%d\n", h->head)
+			 new = h;
+		 	 h = h ->next;
+	 }
 }
+
